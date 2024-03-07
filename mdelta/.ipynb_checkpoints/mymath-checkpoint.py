0e5ec7995528083e4d84f3b098ee8073a3a999b0
Chenjy0212 <1026224216@qq.com>
2024-03-08 06:27:52 +0800

    修复了 mymath 中的计算 bug

--- mdelta/.ipynb_checkpoints/mymath-checkpoint.py
+++ mdelta/.ipynb_checkpoints/mymath-checkpoint.py
@@ -41,33 +41,6 @@ class Greedy_Algorithm:
         return trace
 
 
-'''
-def topscore(mat, root1addr, root2addr, tracemat, num:int=-1):
-    mat_tmp = copy.deepcopy(mat.values)
-    scorelist=[]
-    if num == -1:
-        loopt = min(5,min(mat_tmp.shape))
-    else:
-        loopt = min(min(mat_tmp.shape), num)
-    for _ in range(loopt):
-        #print(mat_tmp)      
-        del_i_index = np.where(mat_tmp==np.max(mat_tmp))[0][0]
-        del_j_index = np.where(mat_tmp==np.max(mat_tmp))[1][0]
-        #scorelist.append([np.max(mat_tmp),mat.index[del_i_index],mat.columns[del_j_index]])
-        #scorelist.append([np.max(mat_tmp), del_i_index, del_j_index])
-        scorelist.append({'Score':np.max(mat_tmp), 
-                          'Root1_label':root1addr[del_i_index].label, 
-                          'Root1_node':root1addr[del_i_index].nodeobj,
-                          'Root2_label':root2addr[del_j_index].label, 
-                          'Root2_node':root2addr[del_j_index].nodeobj, 
-                          'row':del_i_index, 
-                          'col':del_j_index})
-        mat_tmp[del_i_index] = -999.
-        mat_tmp[:,del_j_index] = -999.
-    return scorelist
-'''
-
-
 def GetMaxScore(trace,
                 root1,
                 root2,
@@ -92,69 +65,29 @@ def GetMaxScore(trace,
     key2 = root2node.nodeobj+'_'+root1node.nodeobj
     if root1node.left == None and root2node.left == None:
         # 路径只能是节点直接匹配
-        #if key in dict_score:
-            if float(dict_score[key]) > prune:
-                trace[root1_index][root2_index].append([root1.nodeobj, root2.nodeobj])
-            return float(dict_score[key])
-        #else:
-            #dict_score[key] = score
-            #dict_score[key2] = score
-            #return score
+        if float(dict_score.get(key)) > prune:
+            trace[root1_index][root2_index].append([root1.nodeobj, root2.nodeobj])
+        return float(dict_score[key])
+    
     # 如果只有一个树表示为根节点
     elif root1node.left == None and root2node.left is not None:
-        # if key in dict_score:
-        #    return dict_score[key]
-        if (dict_score.get(root1node.nodeobj+'_'+root1node.nodeobj, prune)+prune*(root2node.leaf_count()-1) < prune):
-            #dict_score[key] = score
-            #dict_score[key2] = score
-            return score
+        dearr = local_matrix.flatten()
+        maxnum = np.max(dearr)
+        if maxnum > prune:
+            trace[root1_index][root2_index].append([root1_index, local_matrix_root2_index[dearr.tolist().index(maxnum)]])
+            return maxnum + prune*(root2node.leaf_count()-1)
         else:
-            dearr = local_matrix.flatten()
-            maxnum = np.max(dearr)
-            score = max(score, maxnum+(root2.leaf_count()-1)*prune)
-            if score > prune:
-                trace[root1_index][root2_index].append(
-                    [root1_index, local_matrix_root2_index[dearr.tolist().index(maxnum)]])
-            # root2node = root2node.left #来到子树下
-            #key_tmp = root1node.nodeobj+'_'+root2node.nodeobj
-            #score = max(score, dict_score[key_tmp])
-            # while root2node.right: #子树还有节点
-            #    root2node = root2node.right
-            #    key_tmp = root1node.nodeobj+'_'+root2node.nodeobj
-            #    score = max(score, dict_score[key_tmp])
-            #score = max(score+((root2.leaf_count()-1)*prune),prune)
-            #dict_score[key] = score
-            #dict_score[key2] = score
-            # return root2.leaf_count()
-            return score
+            return prune*root2node.leaf_count()
 
     elif root1node.left is not None and root2node.left == None:
-        # if key in dict_score:
-        #    return dict_score[key]
-        # print(max(local_matrix))
-        if (dict_score.get(root2node.nodeobj+'_'+root2node.nodeobj, -1.)+prune*(root1node.leaf_count()-1) < prune):
-            #dict_score[key] = score
-            #dict_score[key2] = score
-            return score
+        dearr = local_matrix.flatten()
+        maxnum = np.max(dearr)
+        if maxnum > prune:
+            trace[root1_index][root2_index].append([local_matrix_root1_index[dearr.tolist().index(maxnum)], root2_index])
+            return maxnum + prune*(root1node.leaf_count()-1)
         else:
-            dearr = local_matrix.flatten()
-            maxnum = np.max(dearr)
-            score = max(score, maxnum+(root1.leaf_count()-1)*prune)
-            if score > prune:
-                trace[root1_index][root2_index].append(
-                    [local_matrix_root1_index[dearr.tolist().index(maxnum)], root2_index])
-            # root1node = root1node.left #来到子树下
-            #key_tmp = root1node.nodeobj+'_'+root2node.nodeobj
-            #score = max(score, dict_score[key_tmp])
-            # while root1node.right: #子树还有节点
-            #    root1node = root1node.right
-            #    key_tmp = root1node.nodeobj+'_'+root2node.nodeobj
-            #    score = max(score, dict_score[key_tmp])
-            #score = max(score+((root1.leaf_count()-1)*prune),prune)
-            #dict_score[key] = score
-            #dict_score[key2] = score
-            # return root2.leaf_count()
-            return score
+            return prune*root1node.leaf_count()
+
     # 两个树都是非叶子结点
     else:
         summ = 0.
@@ -187,7 +120,6 @@ def GetMaxScore(trace,
             trace_tmp = []
             cost_matrix = []
             for row in local_matrix:
-                # print(row)
                 cost_row = []
                 for col in row:
                     cost_row += [99999 - col]
@@ -195,29 +127,28 @@ def GetMaxScore(trace,
             m = Munkres()
             indexes = m.compute(cost_matrix)
             # print(indexes)
-            #print_matrix(local_matrix, msg='Highest profit through this local_matrix:')
+
             total = 0
             for row, column in indexes:
                 #print(row, column)
                 value = local_matrix[row][column]
-                total += value
                 if value > prune:
+                    total += value
                     trace_tmp.append(
                         [local_matrix_root1_index[row], local_matrix_root2_index[column]])
-                #print(f'({row}, {column}) -> {value}')
-            #print('total profit= ',total)
-            score = total + \
-                abs(local_matrix.shape[0] - local_matrix.shape[1])*prune
-            if score > min(r1leaf, r2leaf)*prune:
+                else:
+                    total += prune
+                
+            score = total + abs(local_matrix.shape[0] - local_matrix.shape[1])*prune
+            if score > max(r1leaf, r2leaf)*prune:
                 trace[root1_index][root2_index] = trace_tmp
             else:
-                score = min(r1leaf, r2leaf)*prune
+                score = max(r1leaf, r2leaf)*prune
 
         # 遍历
         for i in zip(root1node.son(), local_matrix_root1_index):
             if i[0].left is not None:
-                score_tmp = allmatrix[i[1], root2_index] + \
-                    prune*(r1leaf - i[0].leaf_count())
+                score_tmp = allmatrix[i[1], root2_index] + prune*(r1leaf - i[0].leaf_count())
                 if score < score_tmp:
                     score = score_tmp
                     trace[root1_index][root2_index].clear()
@@ -225,23 +156,17 @@ def GetMaxScore(trace,
                 #score = max(allmatrix[i[1],root2_index] + prune*(r1leaf - i[0].leaf_count()), score)
         for j in zip(root2node.son(), local_matrix_root2_index):
             if j[0].left is not None:
-                score_tmp = allmatrix[root1_index, j[1]] + \
-                    prune*(r2leaf - j[0].leaf_count())
+                score_tmp = allmatrix[root1_index, j[1]] + prune*(r2leaf - j[0].leaf_count())
                 if score < score_tmp:
                     score = score_tmp
                     trace[root1_index][root2_index].clear()
                     trace[root1_index][root2_index].append([root1_index, j[1]])
                 #score = max(allmatrix[root1_index,j[1]] + prune*(r2leaf - j[0].leaf_count()), score)
 
-        # print()
+        # merge 融合中间节点
         if (abs(merge - (100)) < 1e-10):
-            #print('不merge')
             pass
         else:
-            # print("merge:",merge)
-            #print(root1.label, root1.node_count(), root2.label, root2.node_count())
-            #print(root1.node_count() - root1.leaf_count() - 1, root2.node_count() - root2.leaf_count() - 1)
-            #print(root1.nodeobj, root2.nodeobj)
             root1_leaves_nodeobj = []
             root2_leaves_nodeobj = []
             root1_leaves_label = []
@@ -253,10 +178,6 @@ def GetMaxScore(trace,
             for j in root2.leaves([]):
                 root2_leaves_nodeobj.append(j.nodeobj)
                 root2_leaves_label.append(j.label)
-                # print('Leav：',j.label)
-            # print(root1_leaves_label_nodeobj)
-            # print(root1_leaves_label)
-            # print(root2_leaves_label_nodeobj)
 
             a = [x for x in root1_leaves_nodeobj if x in root2_leaves_nodeobj]
             b = [root1_leaves_label[x]
